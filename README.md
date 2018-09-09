9101f4a9921bed0a693d9ec70b9f686c342c143c
Jill Morimoto <jillmorimoto@gmail.com>
2018-09-08 20:03:45 -0700

    1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FriendFinder
+
+The Friend Finder application is basically a dating app, but for making new friends. This full-stack site will take in survey answers, then compare those answers with data from other users. The app will then display the name and picture of the user with the best overall match.
+
---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+[
+    {
+    "name": "Alexander Hamilton",
+    "photo": "https://media.giphy.com/media/d4bmtcUmgA8ylgCk/giphy.gif",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "3",
+    "2",
+    "5",
+    "1",
+    "4",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Aaron Burr",
+    "photo": "https://media.giphy.com/media/DMWABpT3VMzXW/giphy.gif",
+    "scores": [
+    "3",
+    "5",
+    "4",
+    "4",
+    "2",
+    "5",
+    "5",
+    "4",
+    "3",
+    "4"
+    ]
+    },
+    {
+    "name": "Angelica Schuyler",
+    "photo": "https://media.giphy.com/media/JXvN0AvZTpD2w/giphy.gif",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Eliza Schuyler-Hamilton",
+    "photo": "https://media.giphy.com/media/gVFcgF9kJ1fUI/giphy.gif",
+    "scores": [
+    "3",
+    "2",
+    "4",
+    "1",
+    "5",
+    "3",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "King George",
+    "photo": "https://media.giphy.com/media/3ov9jNzt9SRTaH06SA/giphy.gif",
+    "scores": [
+    "3",
+    "1",
+    "3",
+    "5",
+    "1",
+    "5",
+    "1",
+    "1",
+    "1",
+    "5"
+    ]
+    },
+]
